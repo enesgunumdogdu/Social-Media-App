de52457bf5da58e78f11ba8e72e1422930eb6e0d
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-05-26 13:10:58 +0300

    create account page

--- app/src/main/java/com/enesgunumdogdu/kotlininstagram/MainActivity.kt
+++ app/src/main/java/com/enesgunumdogdu/kotlininstagram/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.enesgunumdogdu.kotlininstagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -23,5 +24,10 @@ class MainActivity : AppCompatActivity() {
 
     fun signUpClicked(view : View){
 
+    }
+
+    fun createAccount(view : View){
+        val intent = Intent(this,CreateAccountActivity::class.java)
+        startActivity(intent)
     }
 }
