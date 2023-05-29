7713c89a4308f234ea99ac32a4cda3423e36105a
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-05-29 13:48:25 +0300

    login & sign up

--- app/src/main/java/com/enesgunumdogdu/kotlininstagram/CreateAccountActivity.kt
+++ app/src/main/java/com/enesgunumdogdu/kotlininstagram/CreateAccountActivity.kt
@@ -6,12 +6,16 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import com.enesgunumdogdu.kotlininstagram.databinding.ActivityCreateAccountBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class CreateAccountActivity : AppCompatActivity() {
     private lateinit var binding : ActivityCreateAccountBinding
-
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,7 @@ class CreateAccountActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        auth = Firebase.auth
 
         binding.usernameEditText.addTextChangedListener(textWatcher)
         binding.emailEditText.addTextChangedListener(textWatcher)
@@ -38,22 +43,32 @@ class CreateAccountActivity : AppCompatActivity() {
 
         if(password.isEmpty()){
             binding.passwordEditText.error = "Password cannot be empty!"
+            return
         }
 
         if(!isEmailValid(email)){
             binding.emailEditText.error = "Invalid email adress!"
             return
         }
+        else{
+            // all of them valid
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener {
+                Toast.makeText(this@CreateAccountActivity,"Your account successfully created!",Toast.LENGTH_LONG).show()
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this@CreateAccountActivity,it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
 
-        val intent = Intent(this,MainActivity::class.java)
-        startActivity(intent)
+        }
     }
+
 
     private fun isEmailValid(email:String):Boolean{
         val pattern = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}".toRegex()
         return email.matches(pattern)
     }
-
 
 
     private val textWatcher = object: TextWatcher{
--- app/src/main/java/com/enesgunumdogdu/kotlininstagram/MainActivity.kt
+++ app/src/main/java/com/enesgunumdogdu/kotlininstagram/MainActivity.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.enesgunumdogdu.kotlininstagram.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +20,36 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        auth = Firebase.auth
 
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            val intent = Intent(this,FeedActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
 
     }
 
-    fun signInClicked(view : View){
 
+
+    fun signInClicked(view : View){
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordText.text.toString()
+
+        if(email.equals("") || password.equals("")){
+            Toast.makeText(this,"Invalid email or password!",Toast.LENGTH_LONG).show()
+        }else{
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+                Toast.makeText(this@MainActivity,"Successfully logged in!",Toast.LENGTH_LONG).show()
+                val intent = Intent(this@MainActivity,FeedActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener {
+                Toast.makeText(this@MainActivity,it.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     fun signUpClicked(view : View){
