a0e7a2c2e751ae8c984fb9ceba91ab3a9d3fcbeb
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2023-06-09 14:30:47 +0300

    get data func

--- app/src/main/java/com/enesgunumdogdu/kotlininstagram/FeedActivity.kt
+++ app/src/main/java/com/enesgunumdogdu/kotlininstagram/FeedActivity.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.enesgunumdogdu.kotlininstagram.databinding.ActivityFeedBinding
+import com.enesgunumdogdu.kotlininstagram.model.Post
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFeedBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private lateinit var postArrayList : ArrayList<Post>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +27,12 @@ class FeedActivity : AppCompatActivity() {
         setContentView(view)
 
         auth = Firebase.auth
+        db = Firebase.firestore
+
+        postArrayList = ArrayList<Post>()
+
+        getData()
+
         binding.bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.home -> {
@@ -40,6 +52,33 @@ class FeedActivity : AppCompatActivity() {
                     true
                 }
                 else -> false
+            }
+        }
+    }
+
+    private fun getData(){
+        db.collection("Posts").addSnapshotListener { value, error ->
+
+            if(error != null){
+                Toast.makeText(this,error.localizedMessage,Toast.LENGTH_LONG).show()
+            }else{
+                if(value !=null){
+                  if(!value.isEmpty){
+                      val documents = value.documents
+
+                      for(document in documents){
+                          val comment = document.get("comment") as String
+                          val userEmail = document.get("userEmail") as String
+                          val downloadUrl = document.get("downloadUrl") as String
+
+                          println(comment)
+
+                          val post = Post(userEmail,comment,downloadUrl)
+                          postArrayList.add(post)
+
+                      }
+                  }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/enesgunumdogdu/kotlininstagram/model/Post.kt
@@ -0,0 +1,5 @@
+package com.enesgunumdogdu.kotlininstagram.model
+
+data class Post(val email: String, val comment: String, val downloadUrl: String) {
+
+}
